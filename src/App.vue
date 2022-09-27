86e00156babf596f35c4f0199f7edee7a2485f04
Mustafa Sagdic <mmsagdic@gmail.com>
2022-09-28 01:55:33 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="site">
+      <div class="name">To-Do List</div>
+      <div class="description">I know, you'll never use it but hey, that's a to-do list.</div>
+    </div>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+    </nav>
+  </header>
+  <main>
+    <RouterView />
+  </main>
+  
+</template>
+
+<style lang="scss" scoped>
+@import url(https://fonts.googleapis.com/css?family=Poppins:100,100italic,200,200italic,300,300italic,regular,italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic);
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font: {
+    family: 'Poppins', sans-serif;
+  }
+
+  &::selection {
+    background: transparentize(#42B883, 0.5);
+  }
+}
+
+a {
+  text-decoration: none;
+}
+
+header {
+  background: transparentize(#4275b8, 0.5);
+  padding: .5rem 0 .5rem 0;
+  border-radius: .5rem;
+  .site {
+    display: flex;
+    justify-content: space-evenly;
+
+    .name {
+      font-weight: 500;
+    }
+  }
+
+  nav {
+    display: flex;
+    justify-content: space-around;
+    margin: .25rem 0 .25rem 0;
+    padding: .25rem;
+    border-radius: .5rem;
+
+    a {
+      color: rgb(250, 250, 250);
+    }
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,132 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <div class="add-todo">
+      <form @submit.prevent="addTodo" class="add-box">
+        <input v-model="todoInput" type="text" placeholder="Add Task" minlength="5" autofocus />
+        <input type="submit" value="Add" />
+      </form>
+    </div>
+    <div class="todos">
+      <div v-for="todo in todos" :key="todo.id" class="todo-card" :class="{ done: todo.done }" @click="toggleStatus(todo)">
+        <h2>{{todo.taskName}}</h2>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+  import { ref } from 'vue';
+
+  export default {
+    setup() {
+      const todoInput = ref('');
+      const todos = ref([]);
+
+      const addTodo = () => {
+        if(todoInput.value != "") {
+          todos.value.push({
+            done: false,
+            taskName: todoInput.value,
+            id: Date.now(),
+          });
+          todoInput.value = '';
+        }
+        saveTodos();
+      }
+
+      const loadTodos = () => {
+        console.log('here');
+        const values = JSON.parse(localStorage.getItem('todos'));
+        if(values) todos.value.push(...values);
+      }
+
+      const saveTodos = () => {
+        localStorage.setItem('todos', JSON.stringify(todos.value));
+      }
+
+      const toggleStatus = (todo) => {
+        todo.done = !todo.done;
+        saveTodos();
+      }
+
+      return {
+        todoInput,
+        todos,
+        addTodo,
+        toggleStatus,
+        loadTodos,
+      }
+    },
+    created() {
+      this.loadTodos()
+    }
+  }
+</script>
+
+<style lang="scss">
+body {
+  background: linear-gradient(45deg, #7d84b2 0%, lighten(#7d84b2, 7%) 100%);
+}
+
+.add-todo {
+  background-color: rgba(250, 235, 215, 0.63);
+  padding: .5rem;
+  margin: .75rem .5rem .75rem .5rem;
+  border-radius: .5rem;
+
+  form {
+    display: flex;
+    justify-content: space-evenly;
+
+    input[type="submit"] {
+      margin: 0 .5rem 0 .5rem;
+    }
+
+    input[type="text"] {
+      width: 100%;
+      border-radius: .25rem;
+      padding: .125rem;
+      background-color: transparent;
+      border: none;
+
+      &:focus, &:active, &:focus-visible {
+        border: none;
+        outline: none;
+      }
+    }
+  }
+}
+
+.todos {
+display: flex;
+flex-direction: column-reverse;
+}
+
+.todo-card {
+  border: 1px solid rgba(204, 204, 204, 0.5);
+  border-radius: .25rem;
+  margin: .5rem 1rem .5rem 1rem;
+  padding: 0 .25rem 0 .25rem;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  width: fit-content;
+
+  transition: filter, background-color, color 0.2s ease-in;
+
+  h2 {
+    font-size: 1rem;
+    font-weight: normal;
+  }
+
+  &.done {
+    filter: blur(2px) opacity(50%);
+    background-color: lightgreen;
+    color: white;
+  }
+}
+</style>
